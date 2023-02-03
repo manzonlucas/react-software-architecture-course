d5e9ec316447cf02f9b4fe2b6433e7ab286f4089
manzonlucas <manzonlucasm@gmail.com>
2023-02-03 16:51:52 -0300

    add setup for adding components for ssr

--- module02/src/server.js
+++ module02/src/server.js
@@ -1,16 +1,25 @@
+// npx babel-node src/server.js
+// npx nodemon --exec npx babel-node src/server.js
+
 import express from 'express';
 import React from "react";
 // this will do the actual app rendering:
 import { renderToString } from "react-dom/server";
+import { Home } from '../src/pages/Home';
 
 const app = express();
 const port = 8080;
+
+// tells express to static serve the files inside that folder
+// For default if the specified folder has an index.html, that's the one it's going to serve
+// index: false, prevents this, so we can use our index.html
+app.use(express.static('./build', { index: false }));
 
 // catch ALL the get requests on any path
 app.get('/*', (req, res) => {
   // convert JSX into HTML string:
   const reactApp = renderToString(
-    <h1>Hello from the server side</h1>
+    <Home />
   );
 
   return res.send(`
